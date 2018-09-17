c94d146dce3d676bec9989935954bc0a5c71b855
wuchuanchuan <superwucc@gmail.com>
2018-09-17 06:40:37 +0000

    邮箱验证

--- resources/views/pages/email_verify_notice.blade.php
+++ resources/views/pages/email_verify_notice.blade.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: wuchuanchuan
- * Date: 2018/9/17
- * Time: 下午1:33
- */
+@extends('layouts.app')
+
+@section('title', '提示')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">提示</div>
+        <div class="panel-body text-center">
+            <h1>请先验证邮箱</h1>
+            <a class="btn btn-primary" href="{{ route('root') }}">返回首页</a>
+        </div>
+    </div>
+@stop
