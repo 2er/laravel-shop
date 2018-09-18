be8db886dcc0340331f043f5b40f0f9ef4356d74
wuchuanchuan <superwucc@gmail.com>
2018-09-18 11:20:09 +0000

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserAddressRequest;
+use App\Models\UserAddress;
 
 class UserAddressesController extends Controller
 {
-    public function index(Request $request)
+    public function index(UserAddressRequest $request)
     {
         $addresses = $request->user()->addresses;
         return view('user_addresses.index', compact('addresses'));
+    }
+
+    public function create(UserAddress $address)
+    {
+        return view('user_addresses.create_and_edit', compact('address'));
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('select-district', require('./components/SelectDistrictComponent.vue'));
+Vue.component('user-addresses-create-and-edit', require('./components/UserAddressesCreateAndEdit.vue'));
 
 const app = new Vue({
     el: '#app'
