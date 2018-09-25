b465ae73cbc9e0524cadd3d2af576ce50ab9aec4
wuchuanchuan <superwucc@gmail.com>
2018-09-25 02:47:21 +0000

    用户界面-订单详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
-        \App\Models\UserAddress::class => \App\Policies\UserAddressPolicy::class
+        \App\Models\UserAddress::class => \App\Policies\UserAddressPolicy::class,
+        \App\Models\Order::class => \App\Policies\OrderPolicy::class
     ];
 
     /**
