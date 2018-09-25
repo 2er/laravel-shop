8e8056a35c94448985d8291887aed9585f89aafa
wuchuanchuan <superwucc@gmail.com>
2018-09-25 07:05:01 +0000

    订单支付

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\InvalidRequestException;
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function payByAlipay(Order $order, Request $request)
+    {
+        // 判断当前订单是否属于当前用户
+        $this->authorize('own', $order);
+
+        // 订单是否已支付或者已关闭
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        // 调用支付宝网页支付
+        return app('alipay')->web([
+            'out_trade_no' => $order->no,
+            'total_amount' => $order->total_amount,
+            'subject' => '支付 Laravel Shop 的订单：'.$order->no
+        ]);
+    }
+
+    public function alipayReturn()
+    {
+        try {
+
+            app('alipay')->verify();
+
+        } catch (\Exception $e) {
+
+            return view('pages.error', ['msg' => '数据不正确']);
+
+        }
+
+        return view('pages.success', ['msg' => '支付成功']);
+    }
+
+    public function alipayNotify()
+    {
+        // 校验输入参数
+        $data = app('alipay')->verify();
+
+        $order = Order::where('no', $data->out_trade_no)->first();
+
+        if (!$order) {
+            return 'fail';
+        }
+
+        if ($order->paid_at) {
+            return app('alipay')->success();
+        }
+
+        $order->update([
+            'paid_at' => Carbon::now(),
+            'payment_method' => 'alipay',
+            'payment_no' => $data->trade_no
+        ]);
+
+        return app('alipay')->success();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,10 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('orders', 'OrdersController@index')->name('orders.index');
         // 订单详情
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+        // 订单支付（支付宝网页支付）
+        Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+        // 支付宝支付前端回调
+        Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     });
 });
 
@@ -64,3 +68,5 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('products', 'ProductsController@index')->name('products.index');
 // 商品详情
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+// 支付宝服务器端通知
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
